db3d5189503cba29cb38b9dbe46bc9d6002d9978
Dinis Baltazar Marques <dbaltaza@c1r3s3.42lisboa.com>
2025-11-05 12:38:39 +0000

    ft_printf

--- test.c
+++ test.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dbaltaza <dbaltaza@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: dbaltaza <dbaltaza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 17:56:04 by dbaltaza          #+#    #+#             */
-/*   Updated: 2025/11/03 18:01:05 by dbaltaza         ###   ########.fr       */
+/*   Updated: 2025/11/05 12:38:19 by dbaltaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,13 @@
 
 int main(void)
 {
+
+    int *ptr = NULL;
     printf("Simple test:\n");
-    ft_printf("%u\n", 42);
-    ft_printf("%u %u\n", 42, 100);
-    ft_printf("%u %u %u\n", 1, 2, 3);
+    ft_printf("%p\n", ptr);
     
     printf("\nExpected:\n");
-    printf("%u\n", 42);
-    printf("%u %u\n", 42, 100);
-    printf("%u %u %u\n", 1, 2, 3);
+    printf("%p\n", ptr);
     
     return 0;
 }
